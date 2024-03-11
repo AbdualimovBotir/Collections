5f2d2ad7ee2f3d50c1dc68c74ce7d16f171b26ca
AbdualimovBotir <abdualimovbotir07@gmail.com>
2024-03-11 15:06:26 +0500

    qwertyu

---
+++ Collections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace Collections
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] massiv = { 3, - 3, 9, - 9, 0 };
+			int count = 0;
+			for (int i = 1; i < massiv.Length; i++)
+			{
+				if ((massiv[i - 1] > 0 && massiv[i]<0) || (massiv[i - 1] < 0 && massiv[i] > 0))
+				{
+					count++;
+					massiv[i - 1] *= -1;
+					massiv[i] *= -1;
+				}
+				else
+				{
+					count++;
+				}
+			}
+            Console.WriteLine(string.Join(",",massiv));
+            Console.WriteLine(count);
+        }
+	}
+}
